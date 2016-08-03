6bb57988ed228926b3120d58178f5f03dde290a3
Sonia Timberlake <soniat@abvitro.com>
2016-08-03 13:39:21 -0400

    runs , guards against over-write, but does not create something that can be re-imported

--- data_import/convert_exported_notebook_scriptpy_to_ipynb.py
+++ data_import/convert_exported_notebook_scriptpy_to_ipynb.py
@@ -1,13 +1,21 @@
 # adapted very slightly from http://stackoverflow.com/questions/23292242/converting-to-not-from-ipython-notebook-format (alexis)
 
-from IPython.nbformat import v3, v4
+#from IPython.nbformat import v3, v4
+from nbformat import v3, v4
 import sys
+import os
+from time import time
 
 if len(sys.argv) != 2 or not sys.argv[1].endswith('.py'):
     sys.exit('supply the .py to convert to notebook')
-
-
+    
 inpy =  sys.argv[1]
+outpynb = sys.argv[1].replace('.py','.ipynb')
+
+# if file exists, make a backup with timestamp
+if os.path.exists(outpynb):
+    os.rename(outpynb, '{}.{:.0f}.{}'.format(outpynb , time() , 'bak'))
+
 
 with open(inpy) as fpin:
     text = fpin.read()
@@ -21,5 +29,5 @@ with open(inpy) as fpin:
     nbook = v4.upgrade(nbook)  # Upgrade v3 to v4
 
     jsonform = v4.writes(nbook) + "\n"
-    with open( inpy+'nb', "w") as fpout:
+    with open( outpynb, "w") as fpout:
         fpout.write(jsonform)
