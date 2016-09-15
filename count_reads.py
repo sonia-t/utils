e1a449039a018ab9413a49c9fc59fd9a30bd4a74
Sonia Timberlake <[ommitted]>
2016-09-15 12:17:57 -0400

    tested

--- count_reads.py
+++ count_reads.py
@@ -1,5 +1,5 @@
 '''
-fastq utils - remember not to reinvent the wheel! 
+fastq utils 
 '''
 __author__ = "Sonia Timberlake"
 
@@ -7,7 +7,9 @@ import os
 import sys
 import argparse
 from os.path import basename, dirname, join
+from Bio import SeqIO
 
+default_delimiter={'fields':' ','key-value':'='}
 def parseAnnotation(record, fields=None, delimiter=default_delimiter):
     """
     Extracts annotations from a FASTA/FASTQ sequence description
@@ -47,30 +49,30 @@ def parse_usearch_header(header,
         field_dict[pairs[0].lower()] = pairs[1]
     return field_dict
 
-def count_fastq(filename, units='reads'):
-    prefix, suffix = basename(filename).split('.', 1)
+def count_fastq(infile, count_unit='reads'):
+    prefix, suffix = basename(infile.name).split('.', 1)
     if len(suffix)==2: suffix = {'fq':'fastq', 'fa':'fasta'}[suffix] # lazy exception handling
-    if not assert(suffix in ('fastq','fasta')):
+    if not suffix in ('fastq','fasta'):
         raise Exception("Encountered file {} in rule counts which doesn't end "
-                        "with fastq, fasta, fa, fq.".format(filename))
+                        "with fastq, fasta, fa, fq.".format(infile.name))
     if 'derep' in prefix and units=='reads': # have to use the size field # TODO: maybe should look for it by default ? 
         header_field = 'size'
         count = sum( int(parse_usearch_header(record.description)[header_field])
-                                 for record in SeqIO.parse(open(str(fname)), suffix) )
+                                 for record in SeqIO.parse(infile, suffix))
     else:
-        count = sum(1 for record in SeqIO.parse(open(str(fname)), suffix))
-    return 
+        count = sum(1 for record in SeqIO.parse(infile, suffix))
+    return count
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(usage=__doc__,
                                      description='''
                                      count reads in a fastq/fasta / usearch file 
                                      ''')
-    parser.add_argument('--infile', required=True,
+    parser.add_argument('--infile', 
                         type=argparse.FileType('r'), default=sys.stdin)
-    parser.add_argument('--outfile', required=True,
+    parser.add_argument('--outfile', 
                         type=argparse.FileType('w'), default=sys.stdout)
-    parser.add_argument('--count_units', choices=["reads","records"],
+    parser.add_argument('--count_unit', choices=["reads","records"],
                         default='reads' , help='in dereplicated fasta, reads!=records')
     parser.add_argument('--test', action='store_true')
     return parser.parse_args(argv)
@@ -80,7 +82,8 @@ def parse_args(argv):
 def main(argv):
     #if not argv: 0
     opts = parse_args(argv[1:])
-    count = count_fastq(opts.infile, opts.count_units)
+    count = count_fastq(opts.infile, opts.count_unit)
+    print("{}\t{}\t{}".format(count,opts.count_unit,opts.infile.name), file=opts.outfile)
     return (count, opts.infile.name)
 
 if __name__ == '__main__':
